6db09a19e91cfd3fdab916c7b13276cd91dba73a
company_youngsik <samechs@naver.com>
2021-02-23 19:23:18 +0900

    Read fix & solve string7

--- baekjoon/string/string_7_2908.c
+++ baekjoon/string/string_7_2908.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
-int(main){
 
-    return 0;
+int main(void) {
+
+	int n1, n2;
+	
+	while(1)
+	{
+		scanf("%d %d", &n1, &n2);
+		if((n1>99)||(n2>99))
+		{
+			break;
+		}	
+	}
+	n1 = n1/100 + ((n1/10)%10)*10 + ((n1%100)%10)*100;
+	n2 = n2/100 + ((n2/10)%10)*10 + ((n2%100)%10)*100;
+
+	if(n1 < n2) printf("%d", n2);
+	else printf("%d", n1);
 }
