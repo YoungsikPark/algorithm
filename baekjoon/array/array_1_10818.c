744f95606096977783b1600c9e1d707aef8b66a3
company_youngsik <samechs@naver.com>
2021-03-31 14:54:56 +0900

    array 1 solve

--- baekjoon/array/array_1_10818.c
+++ baekjoon/array/array_1_10818.c
@@ -1,1 +1,28 @@
-#include "stdio.h"
+#include<stdio.h>
+
+int s,i;
+int min = 1000001;
+int max = -1000001;
+int target = 0;
+
+int main(){
+	while(1){
+		scanf("%d",&s);
+		if((s>0)&&(s<1000000))	{
+			break;
+		 }
+    }
+    
+    for(i=0;i<s;i++)
+    {
+    	scanf("%d", &target);
+
+        if (target < min) {
+            min = target;
+        }
+        if (target > max) {
+            max = target;
+        }
+	}
+	printf("%d %d",min,max);
+}
