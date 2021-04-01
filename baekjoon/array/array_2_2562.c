2a73f30c1e1cc6b07f9268df904e8f6e08fe1282
company_youngsik <samechs@naver.com>
2021-04-01 15:17:38 +0900

    string 2562_solve

---
+++ baekjoon/array/array_2_2562.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int arr[9], big = 0, index;
+    for (int i = 0; i < 9; i++){
+        scanf("%d", &arr[i]);
+        if (arr[i] > big){
+            big = arr[i];
+            index = i;
+        }
+    }
+    printf("%d %d", big, index+1);
+} // 이런건 넘쉽징.
