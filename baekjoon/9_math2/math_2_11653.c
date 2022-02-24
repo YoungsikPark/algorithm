813dd4147d24e04ad7fab698c334057ff5bf5886
company_youngsik <samechs@naver.com>
2022-02-24 11:13:37 +0900

    solve math 1929

---
+++ baekjoon/9_math2/math_2_11653.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+	
+	int N,i,j;
+	
+	scanf("%d",&N);
+	
+	for (i=2;i<=N;i++){
+		while(N % i == 0)
+		{
+			printf("%d\n",i);
+			N /= i;
+		}
+	}
+	
+	return 0;
+}
