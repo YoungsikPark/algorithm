4f6286afdcb62a26acfa838784f2c16f642f1d83
yspark_notebook <samechs@naver.com>
2022-02-26 22:10:05 +0900

    set for 9020

---
+++ baekjoon/9_math2/math_6_9020.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+	int t,r,n;
+
+	scanf("%d",&t);
+	
+	for(r=0;r<t;r++)
+	{
+		while(1){
+			scanf("%d",&n);
+			if((n>3)&&(n<=10000)){
+			break;
+			}
+		}
+		
+	}
+	return 0;
+}
