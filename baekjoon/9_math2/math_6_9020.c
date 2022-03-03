07e788994369422001418f75ad4bf45dd303f352
company_youngsik <samechs@naver.com>
2022-03-03 10:48:35 +0900

    solve 9020

--- baekjoon/9_math2/math_6_9020.c
+++ baekjoon/9_math2/math_6_9020.c
@@ -1,19 +1,28 @@
 #include <stdio.h>
-
-int main(){
-	int t,r,n;
-
-	scanf("%d",&t);
+int main(void)
+{
+	int N, C, i,n, j;
+	int arr[10001]={1,1,0};
 	
-	for(r=0;r<t;r++)
+	for(i=2;i*i<10001;i++)
+		if(arr[i]==0)
+			for(j=i*i;j<10001;j+=i)
+				arr[j] = 1;
+	
+	scanf("%d",&C);
+	
+	
+	for(n=0;n<C;n++)
 	{
-		while(1){
-			scanf("%d",&n);
-			if((n>3)&&(n<=10000)){
-			break;
-			}
-		}
+		scanf("%d",&N);
 		
+		for(i=N/2;i>=2;i--)
+			if((arr[i]==0) &&(arr[N-i]==0))
+			{
+				printf("%d %d\n",i, N - i);
+				break;
+			}
 	}
+	
 	return 0;
 }
