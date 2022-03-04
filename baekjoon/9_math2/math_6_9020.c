2c152b3b39a013c7882ecc67952f7e5b8a8ce134
company_youngsik <samechs@naver.com>
2022-03-04 11:50:02 +0900

    solve 1085

--- baekjoon/9_math2/math_6_9020.c
+++ baekjoon/9_math2/math_6_9020.c
@@ -7,16 +7,13 @@ int main(void)
 	for(i=2;i*i<10001;i++)
 		if(arr[i]==0)
 			for(j=i*i;j<10001;j+=i)
-				arr[j] = 1;
+				arr[j] = 1; //에라토스 테네스의 채 
 	
-	scanf("%d",&C);
-	
-	
+	scanf("%d",&C); // test case 받기 
 	for(n=0;n<C;n++)
 	{
 		scanf("%d",&N);
-		
-		for(i=N/2;i>=2;i--)
+		for(i=N/2;i>=2;i--) // 짝수값만 
 			if((arr[i]==0) &&(arr[N-i]==0))
 			{
 				printf("%d %d\n",i, N - i);
